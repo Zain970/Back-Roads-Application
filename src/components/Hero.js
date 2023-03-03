23e1eee8b2af59608cf5dd436cdfc6595c854012
Zain@Zain-ul-Abidin.com <itszain20@gmail.com>
2023-03-04 02:00:22 +0500

    back original

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,8 +6,6 @@ function Hero() {
             <section className="hero" id="home">
                 <div className="hero-banner">
                     <h1>Backroads App</h1>
-                    <h1>Another service</h1>
-                    <h2>Again service last 1:21</h2>
 
                     <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
